c8161a5fa211149a01139d5474e55ff683995f96
Pratik_Tulkane <pratiktulkane12345@gmail.com>
2021-12-30 14:15:23 +0530

    "first commit"
    "first commit"

--- src/App.js
+++ src/App.js
@@ -2,20 +2,51 @@
 import Navbar from './MyAppComponent.js/Navbar';
 import './App.css';
 import AppForm from './AppForm';
-// import About from './MyAppComponent.js/About';
+import About from './MyAppComponent.js/About';
+import React, { useState } from 'react'
+import Alert from './Alert';
+
+
 function App() {
-        
+        const [mode, setMode] = useState('light');
+        const [alert, setAlert] = useState(null);
+
+        const showAlert = (message,type) =>{
+            setAlert ({
+              msg : message,
+              type : type
+            })
+            setTimeout(() =>{
+              setAlert(null);
+            },1200)
+        }
+
+        const toggleMode = ()=>{
+          if(mode === 'light'){
+            setMode('dark');
+            document.body.style.backgroundColor = '#343434';
+            showAlert("Dark mode has been enabled", "success");
+          }
+          else{
+            setMode('light');
+            document.body.style.backgroundColor = 'white';
+            showAlert("Light mode has been enabled", "success");
+          }
+  }
   return (
     <>
-    {/* <Navbar title = "Pratik-Tech" nav1 = "Home" nav2 = "HELP" /> */}
-    <Navbar />
-    <div className="container my-3">
-          <AppForm heading = "Enter the text "/>
-          {/* <About/> */}
-    </div>
+        <Navbar title = "Pratik-Tech" nav1 = "Home"  mode={mode}  toggleMode={toggleMode}/>
+        {/* <Navbar mode={mode} /> */} 
+        <Alert alert ={alert}/>
+        <div className="container my-3">
+           
+                <AppForm showAlert={showAlert} heading = "Enter the text " mode={mode} />
+              
+                {/* <About/> */}
+        </div>
     </>
-  );
-}
+  )
+  }
 
 export default App;
 
--- src/AppForm.js
+++ src/AppForm.js
@@ -3,18 +3,32 @@ import React,{useState} from 'react'
 export default function AppForm(props) {
         const handleToUpCase = () =>{
             let newText = text.toLocaleUpperCase();
-            setText(newText);        
+            setText(newText); 
+            props.showAlert('Converted to Uppercase' , 'success');       
         }
         const handleToLoCase = () =>{
             let newText = text.toLowerCase();
             setText(newText);        
+            props.showAlert('Converted to Lowercase' , 'success');       
         }
         const handleToClear = () =>{
             // console.log("click"  + text);
             let newText = " ";
             setText(newText);        
+            props.showAlert('Text area has been clear' , 'success');       
         }
-
+        const handleToCopy = () =>{
+            let newText = document.getElementById('myBox');
+            newText.select();
+            navigator.clipboard.writeText(newText.value);
+            props.showAlert('Text has been copied' , 'success');       
+        }
+        
+        const handleToRemoveExtraSpaces = () =>{
+            let newText = text.split(/[ ]+/);
+            setText(newText.join(" "))
+            props.showAlert('Extra spaces has been removed' , 'success');       
+        }
         const handelOnChange = (event) =>{
             setText(event.target.value);
         }
@@ -22,21 +36,23 @@ export default function AppForm(props) {
     const [text,setText] = useState('');
     return (
         <>
-    <div> 
+    <div className='container' style={{color: props.mode==='light'?'#343434':'white'}}> 
         <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <textarea className="form-control" placeholder='Enter the text' value={text} onChange={handelOnChange} id="myBox" rows="8"></textarea>
+            <div className="mb-3" >
+                <textarea className="form-control" placeholder='Enter the text' value={text} onChange={handelOnChange} style={{backgroundColor: props.mode==='light'?'white':'#343434' , color: props.mode==='light'?'black':'white'}} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-1" onClick={handleToUpCase}>Convert to UpperCase</button>
             <button className="btn btn-primary mx-1" onClick={handleToLoCase}>Convert to LowerCase</button>
             <button className="btn btn-primary mx-1" onClick={handleToClear}>Clear All</button>
+            <button className="btn btn-primary mx-1" onClick={handleToCopy}>Copy to clipboard</button>
+            <button className="btn btn-primary mx-1" onClick={handleToRemoveExtraSpaces}>Remove Extra Spaces</button>
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color: props.mode==='light'?'#343434':'white'}}>
             <h1>Your text summary</h1>
             <p>{text.split(" ").length} words</p>
             <p>{0.008 * text.split(" ").length} minutes to read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length >0?text:'Nothing to preview'}</p>
     </div>
     </>
     )
